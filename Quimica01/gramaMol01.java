c5df6cf8fab1b1cbcda232b12b7e3b3332897b6c
Maria Eduarda Teodoro <madsfts@gmail.com>
2023-10-11 17:04:54 -0300

    11/10/2023

---
+++ Quimica01/gramaMol01.java
@@ -0,0 +1,12 @@
+package Quimica01;
+import java.util.Scanner;
+
+public class gramaMol01 {
+    public static void main (String[] args) {
+        Scanner molzinho = new Scanner(System.in);
+
+        
+
+    }
+    
+}
