dd0662deaf99809b0487c63041b8bc03a28e8fd5
Maria Eduarda Teodoro <madsfts@gmail.com>
2023-10-16 14:45:13 -0300

    16/10/2023

---
+++ Quimica01/ultimoSubnivel.java
@@ -0,0 +1,15 @@
+package Quimica01;
+import java.util.Scanner;
+
+public class ultimoSubnivel {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a configuração eletrônica: ");
+        String configuracaoEletronica = scanner.nextLine();
+
+        int ultimoSubnivel = Character.getNumericValue(configuracaoEletronica.charAt(configuracaoEletronica.length()- 2));
+        System.out.println("O último subnível preenchido é " + ultimoSubnivel + "s: ");
+    }
+    
+}
